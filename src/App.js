2fdc43399441490ffa687825ee38131713e4386a
mdobaid311 <mdobaid311@gmail.com>
2022-06-12 14:19:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+import ReactWebChat, { createDirectLine } from 'botframework-webchat';
+import React, { useMemo } from 'react';
 
 function App() {
+  const directLine = useMemo(() => createDirectLine({ token: 'A-SZGw27zNA.Cl3EZoBO5yBaL1KtgVJfEimDNXMdDSwwPeg9IzARiug' }), []);
   return (
     <div className="container">
       <h1>Javascript Chatbot Using Microsoft Azure Bot Services</h1>
-      <iframe
-        src="https://webchat.botframework.com/embed/js-chatbot-by-obaid-bot?s=A-SZGw27zNA.t3uPZGJYJIV4HqS2cTfoXJ4vfaRrvZguLEmM0ghoY-E"
-        // style="min-width: 400px; width: 100%; min-height: 500px;"
-        className="chatbox"
-      ></iframe>
+      <ReactWebChat directLine={directLine} userID="mdobaid311@gmail.com" />
       <h3>Just type the keyword in the message input field and press send.</h3>
       <span>* Example Keywords: <span>
         let, const, while, for loop, if etc *
       </span>
       </span>
+
     </div>
   );
+  // const directLine = useMemo(() => createDirectLine({ token: 'A-SZGw27zNA.Cl3EZoBO5yBaL1KtgVJfEimDNXMdDSwwPeg9IzARiug' }), []);
+
+  // return <ReactWebChat directLine={directLine} userID="mdobaid311@gmail.com" />;
 }
 
 export default App;
